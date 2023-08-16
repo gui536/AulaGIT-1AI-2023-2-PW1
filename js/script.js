9400759c1e0558c07fd5f543c30dfa8104b1c3bf
Prof. Guilherme Carvalho <guilherme.santos536@etec.sp.gov.br>
2023-08-16 15:18:28 -0300

    adicionei o arquivo js. Alterei o arquivo Index.html

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function login(){
+    var email = document.getElementById("frmemail").value
+    var password = document.getElementById("frmpassword").value
+    console.log(email)
+    console.log(password)
+    if(email == "login" && password == "123456" ){
+        alert("Login realizado com sucesso!")
+    }
+    else{
+        alert("Não foi possível realizar o login")
+    }
+}
+
+
+function somar(){
+    var n1 = parseInt(prompt("Digite o primeiro número"))
+    var n2 = parseInt(prompt("Digite o segundo número"))
+    // Essa função devolve o tipo de dado da variável escolhida. 
+    console.log(typeof(n1)) 
+    console.log(typeof(n2)) 
+    var resultado = n1 + n2
+    console.log(resultado)
+}
+
+
+
+//Manipular o DOM
+//Document Object Model
